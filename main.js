f7ee17f64b6e9eef5e6bac10d943326593a32f96
luanfelipe11BTI ribeiro <kreekshotpc69@gmail.com>
2025-05-05 11:57:17 +0000

    exemplos 1 e 2 prontos

---
+++ main.js
@@ -0,0 +1,20 @@
+/*
+ Exemplo1
+ function mostrarAlerta(){
+     let nome = "Luan"
+     alert("Seja bem-vindo(a), "+nome)
+ }
+ 
+ mostrarAlerta()
+ */
+ 
+ /*
+ 
+ Exemplo2
+ function somar(valor1, valor2){
+     alert(valor1 + valor2)
+ 
+ }
+ 
+ somar(20,12)
+ */
