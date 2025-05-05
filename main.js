d7e57f67d828a4fadb9cabc211ac84c9287ddf4c
luanfelipe11BTI ribeiro <kreekshotpc69@gmail.com>
2025-05-05 12:04:10 +0000

    exemplos 5 e 6 prontos

--- main.js
+++ main.js
@@ -43,3 +43,30 @@
  console.log(multiplicacao2)
  console.log(multiplicacao3)
  */
+
+ /* Exemplo 5
+ function parOuImpar(numero){
+     if(numero %2 === 0){
+         return "Par"
+     }
+ 
+     return "Ímpar"
+ }
+ 
+ alert(parOuImpar(5))
+ */
+ /* Exemplo 6
+ 
+ let numero = 10
+ 
+ function aumentarNumero(){
+     numero += 1
+     console.log(numero)
+  }
+ 
+ 
+ const button = document.querySelector("button")
+ button.addEventListener("click", aumentarNumero)
+ 
+ console.log(numero)
+ */
