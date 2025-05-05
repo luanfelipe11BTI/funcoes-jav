6214a61ded1f31413691eeb1729aaa2783fad6cf
luanfelipe11BTI ribeiro <kreekshotpc69@gmail.com>
2025-05-05 11:59:40 +0000

    exemplos 3 e 4 prontos

--- main.js
+++ main.js
@@ -18,3 +18,28 @@
  
  somar(20,12)
  */
+
+ *
+ Exemplo 3
+ function mensagemBoasVindas(nome, cargo){
+     alert(`Seja bem-vindo(a),  ${cargo} ${nome}`)
+ 
+ }
+ 
+ mensagemBoasVindas("Luan" ,"Incrivel")
+ */
+ 
+ 
+ /* Exemplo 4
+ function multiplicacao(numero1, numero2){
+     return numero1 * numero2
+ }
+ 
+ let multiplicacao1 = multiplicacao(6,7)
+ let multiplicacao2 = multiplicacao(9,10)
+ let multiplicacao3 = multiplicacao(12,25)
+ 
+ console.log(multiplicacao1)
+ console.log(multiplicacao2)
+ console.log(multiplicacao3)
+ */
